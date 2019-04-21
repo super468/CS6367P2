8d83259a0a0a92d6ffaeb783f9ac0223ef5c4d2f
super468 <wangtspeter@gmail.com>
2019-04-20 19:09:29 -0500

    first commit

---
+++ src/main/java/edu/utd/Agent.java
@@ -0,0 +1,31 @@
+package edu.utd;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+public class Agent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("Java Agent is running");
+        inst.addTransformer(new ClassFileTransformer() {
+            public byte[] transform(ClassLoader classLoader, String className, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
+                System.out.println("transforming " + className);
+                // ASM Code
+                if (className.startsWith("org/apache/commons/lang3")
+                ){
+                    System.out.println(className);
+                    ClassReader cr = new ClassReader(bytes);
+                    ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+                    ClassTransformVisitor ca = new ClassTransformVisitor(cw);
+                    cr.accept(ca, 0);
+                    return cw.toByteArray();
+                }
+                return bytes;
+            }
+        });
+    }
+}
---
+++ src/main/java/edu/utd/CoverageCollection.java
@@ -0,0 +1,21 @@
+package edu.utd;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+
+public class CoverageCollection {
+    public static HashMap<String, HashMap<String, LinkedHashSet<Integer>>> testSuite;
+    public static HashMap<String, LinkedHashSet<Integer>> testCase;
+    public static String testName;
+
+    public static void visitLine(String className, Integer line){
+        if(testCase == null || className == null) return;
+        LinkedHashSet<Integer> set = testCase.get(className);
+        if(set == null) {
+            set = new LinkedHashSet<Integer>();
+        }
+        set.add(line);
+        testCase.put(className, set);
+    }
+
+}
